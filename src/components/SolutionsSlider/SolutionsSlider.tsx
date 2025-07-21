8d20b8902c5a1ed79088811315e9d2e5bdac1a96
Dany <dany@mac.home>
2025-07-21 12:40:26 +0200

    fix es lint

--- src/components/SolutionsSlider/SolutionsSlider.tsx
+++ src/components/SolutionsSlider/SolutionsSlider.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { SetStateAction, useState } from 'react';
+import { useState } from 'react';
 import Image from 'next/image';
 import styles from './SolutionsSlider.module.css';
 import Logo from '../Logo/Logo';
